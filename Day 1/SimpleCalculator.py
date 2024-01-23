90de662022000509e6442ab0b55b0578d928f5ea
maziz07 <102008721@student.swin.edu.au>
2024-01-23 23:01:56 +1100

    day 1 programs

---
+++ Day 1/SimpleCalculator.py
@@ -0,0 +1,15 @@
+# Taking user input
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number: "))
+
+# Performing basic arithmetic operations
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+quotient = number1 / number2
+
+# Displaying the results
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
---
+++ Day 1/helloworld.py
@@ -0,0 +1,4 @@
+# This is a comment in Python. It's used to explain the code.
+
+# Print function displays the text in the console.
+print("Hello, World!")
